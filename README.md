50fa1f8a664766ec5526b76a91904207ec373433
BabyCattleyaGP <bcattleya@gmail.com>
2021-02-16 13:34:35 +0700

    First Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Description
+
+First run build ios app with SwiftUI
+
+## Dev Environment
+
+- Swift version 5.3.1
+- XCode 12.2
---
+++ firstrun/firstrunApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstrunApp.swift
+//  firstrun
+//
+//  Created by Baby Permatagama on 16/02/21.
+//
+
+import SwiftUI
+
+@main
+struct firstrunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
