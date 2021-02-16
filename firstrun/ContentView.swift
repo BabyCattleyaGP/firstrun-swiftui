c9f201d1bdf288e6b68009bb09292e6adcbf6469
BabyCattleyaGP <bcattleya@gmail.com>
2021-02-16 14:35:21 +0700

    Add image, use stack & spacer

--- firstrun/ContentView.swift
+++ firstrun/ContentView.swift
@@ -2,10 +2,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Baby!")
-            .padding()
-            .background(Color.yellow)
-            .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+        VStack{
+            Spacer()
+            Image("logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Spacer()
+            HStack{
+                Text("Hello")
+                    .padding()
+                    .background(Color.yellow)
+                    .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                Text("Baby!").padding()
+            }
+            Spacer()
+        }
     }
 }
 
